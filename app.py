53136c7d44ff79c79cc5f7c25d4095d270c0e93e
hexagonpropltd-nuel <hexagonpropltd@gmail.com>
2025-08-06 13:01:21 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,17 @@
+python
+from flask import Flask, request, jsonify
+import pandas as pd
+import joblib
 
+app = Flask(__name__)
+model = joblib.load('model.joblib')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.get_json(force=True)
+    df = pd.DataFrame(data)
+    prediction = model.predict(df)
+    return jsonify(prediction.tolist())
+
+if __name__ == '__main__':
+    app.run(debug=True)
